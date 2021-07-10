c6821799b6b83d7bf1bc0dfc2fa462b26c3cd5c9
Hillary Okello <hillariouskelly@outlook.com>
2021-07-10 19:58:05 +0300

    fix color contrast

---
+++ build/precache-manifest.8036fe698a0586215d2222fb20c33dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9b23b7a730e66a6b89c04716c8a7de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7903aa07ceb4650f8e7",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7903aa07ceb4650f8e7",
+    "url": "/static/js/main.90900c81.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
